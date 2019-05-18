f8637ec8e3e52843acacd6ffd6fd7c1d7e7588b4
ConnorMacKenzie <connormack97@gmail.com>
2019-05-18 16:14:25 -0400

    second query base

--- itrade-backend/services/json_service.py
+++ itrade-backend/services/json_service.py
@@ -9,3 +9,18 @@ def serialize_per_month(data, month, buysell):
             "average_price": float(round(i[0], ndigits=2))
         })
     return dto
+
+def serialize_per_product(data, month, buysell):
+    dto = {
+        "month": month,
+        buysell: []
+    }
+    for i in data:
+        dto[buysell].append({
+            "id": i[2],
+            "category":{
+                "average_price": float(round(i[0], ndigits=2)),
+                "name":""
+            }
+        })
+    return dto

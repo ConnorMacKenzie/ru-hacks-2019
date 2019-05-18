eacfbe709502576490e2cc307bd5f47b55d27c60
Brendan Lucas <brendanwflucas@gmail.com>
2019-05-18 18:25:32 -0400

    Added flask-cors

--- itrade-backend/controller.py
+++ itrade-backend/controller.py
@@ -2,13 +2,17 @@ from services.database_services import price_per_seller_per_month
 from services.json_service import serialize_per_month
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
     return "Hit me with that dashboard"
 
+
 @app.route('/trends/supplier/<month>')
+@cross_origin()
 def get_supplier_trend(month):
     data = price_per_seller_per_month([month])
     return jsonify(serialize_per_month(data, month, "sellers"))
